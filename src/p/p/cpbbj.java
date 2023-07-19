7592597ff50e5957fd982d820e1f2378018427a1
qxz41659 <119279766+qxz41659@users.noreply.github.com>
2023-07-19 20:50:53 +0800

    Add files via upload

--- src/p/p/cpbbj.java
+++ src/p/p/cpbbj.java
@@ -1,60 +1,88 @@
 package p.p;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class cpbbj {
-    public boolean chineseZodiacSigns(String[] names,String[] chineseZodiacSigns){
-        if (names.length != chineseZodiacSigns.length) {
-            return false;
-        }
-        boolean pppppp = false;
-        for (int i = 0; i < names.length; i++) {
-            switch (chineseZodiacSigns[i]) {
-                case "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪", "屁" -> {
-                    pppppp = true;
-                    break;
-                }default -> pppppp = false;
-            }
-            if(!pppppp){
-                return false;
-            }
-        }
-        return pppppp;
+    static Scanner sc = new Scanner(System.in);
+    static Random r = new Random();
+    public static void start(){
+        System.out.println("v4.0.0");
+        System.out.println("此程序可以测试你屁不屁，而你只需要输入姓名和生肖");
+        System.out.println("你可以输入一串数据");
+        System.out.println("用中文逗号隔开");
+        System.out.println("请选择你的方法");
+        areYouP();
     }
-    public String WhoIsP(AreYouP p){
-        Random r = new Random();
-        if(p.getName().equals("屁") && p.getChineseZodiacSigns().equals("屁")) {
-            return "啊！你如此闲得慌的输入了两个屁！你可真屁！史无前例！";
-        }else if(p.getName().equals("屁") || p.getChineseZodiacSigns().equals("屁")) {
-            return "啊！你竟然往个人信息中填“屁”，你可真屁！";
-        }else if(p.getName().equals("曲晓贺") && p.getChineseZodiacSigns().equals("蛇")) {
-            return "啊！" + p.getName() + "！你可真是非常非常非常屁啊！";
-        }else if(p.getName().equals("曲霄致") && p.getChineseZodiacSigns().equals("兔")) {
-            return "啊！" + p.getName() + "！你可太不屁啦！史无前例！";
-        }else if(p.getName().equals("曲晓贺") && !p.getChineseZodiacSigns().equals("蛇")) {
-            return "啊！" + p.getName() + "！你生肖不屁，名字很屁";
-        }else if(p.getName().equals("曲霄致") && !p.getChineseZodiacSigns().equals("兔")) {
-            return "啊！" + p.getName() + "！你名字不屁，生肖很屁";
-        }else {
-            int o = r.nextInt(3);
-            switch (o) {
-                case 0 -> {
-                    return "你不屁";
+    private static void areYouP(){
+        while (true) {
+            System.out.println("1.生肖测屁");
+            System.out.println("2.星座测屁");
+            String command = sc.next();
+            switch (command) {
+                case "1", "2" -> {
+                    System.out.println("请输入你的姓名");
+                    String[] name = sc.next().split("，");
+                    System.out.println("请输入你的" + (command.equals("1") ? "生肖" : "星座"));
+                    String[] temp = sc.next().split("，");
+                    boolean temp1 = p(name, temp);
+                    if (temp1) {
+                        System.out.println("输错啦！");
+                    } else {
+                        areYouP(name, temp);
+                    }
                 }
-                case 1 -> {
-                    return "你有点屁";
-                }
-                case 2 -> {
-                    return "你很屁";
+                default -> {
+                    System.out.println("鉴定完毕，你是屁");
                 }
             }
         }
-        return null;
     }
-    //为什么要把api放这？？？
-    //直接调不行吗？？？？
-    //算了，懒得改了。
-    public String[] NameAndChineseZodiacSigns(String data){
-        return data.split("，");
+    private static boolean p(String[] name,String[] data){
+        if (name.length != data.length){
+            return true;
+        }
+        boolean temp = true;
+        for (String datum : data) {
+            switch (datum) {
+                case "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座", "屁" -> {
+                    temp = false;
+                }
+                default -> {
+                    temp = true;
+                }
+            }
+        }
+        return temp;
+    }
+    private static void areYouP(String[] name,String[] data){
+        for (int i = 0;i < name.length;i++) {
+            if(name[i].equals("屁") && data[i].equals("屁")) {
+                System.out.println("啊！你如此闲得慌的输入了两个屁！你可真屁！史无前例！");
+            }else if(name[i].equals("屁") || data[i].equals("屁")) {
+                System.out.println("啊！你竟然往个人信息中填“屁”，你可真屁！");
+            }else if(name[i].equals("曲晓贺") && (data[i].equals("蛇") || data[i].equals("天蝎座"))) {
+                System.out.println("啊！" + name[i] + "！你可真是非常非常非常屁啊！");
+            }else if(name[i].equals("曲霄致") && (data[i].equals("兔") || data[i].equals("白羊座"))) {
+                System.out.println("啊！" + name[i] + "！你可太不屁啦！史无前例！");
+            }else if(name[i].equals("曲晓贺") && (!data[i].equals("蛇") || !data[i].equals("天蝎座"))) {
+                System.out.println("啊！" + name[i] + "！你名字很屁，生肖或星座不屁！");
+            }else if(name[i].equals("曲霄致") && (!data[i].equals("兔") || !data[i].equals("白羊座"))) {
+                System.out.println("啊！" + name[i] + "！你名字不屁，生肖或星座很屁！");
+            }else {
+                int o = r.nextInt(3);
+                switch (o) {
+                    case 0 -> {
+                        System.out.println("你不屁");
+                    }
+                    case 1 -> {
+                        System.out.println("你有点屁");
+                    }
+                    case 2 -> {
+                        System.out.println("你很屁");
+                    }
+                }
+            }
+        }
     }
 }
--- src/p/p/p.java
+++ src/p/p/p.java
@@ -1,42 +1,6 @@
 package p.p;
-
-import java.util.Objects;
-import java.util.Scanner;
-
 public class p {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        AreYouP p = new AreYouP();
-        cpbbj pp = new cpbbj();
-        System.out.println("v3.0.1");
-        System.out.println("此程序可以测试你屁不屁，而你只需要输入姓名和生肖");
-        System.out.println("你可以输入一串数据");
-        System.out.println("用中文逗号隔开");
-        while (true) {
-            System.out.println("请输入你的姓名");
-            p.setName(sc.next());
-            String[] names = pp.NameAndChineseZodiacSigns(p.getName());
-            String[] ChineseZodiacSigns;
-            P:
-            while (true) {
-                System.out.println("请输入你的生肖");
-                p.setChineseZodiacSigns(sc.next());
-                ChineseZodiacSigns = pp.NameAndChineseZodiacSigns(p.getChineseZodiacSigns());
-                if(pp.chineseZodiacSigns(names,ChineseZodiacSigns)){
-                    break P;
-                }else{
-                    System.out.println("输错啦！重输！");
-                        }
-                    }
-            for (int i = 0; i < names.length; i++) {
-                p.setName(names[i]);
-                p.setChineseZodiacSigns(ChineseZodiacSigns[i]);
-                System.out.println(pp.WhoIsP(p));
-            }
-            System.out.println("键入任意键退出，键入1继续测试");
-            if (!Objects.equals(sc.next(), "1")) {
-                return;
-            }
-        }
+    public static void main(String[] args){
+        cpbbj.start();
     }
 }
